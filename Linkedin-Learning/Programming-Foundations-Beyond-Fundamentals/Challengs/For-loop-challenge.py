6e9b6af0e1db672f7584cfd7636d6fc5ae0dc82f
Yasin samy <78241714+y3sin@users.noreply.github.com>
2021-10-21 09:57:50 -0700

    Update For-loop-challenge.py

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/For-loop-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/For-loop-challenge.py
@@ -1,1 +1,12 @@
+fruits = [                    # Declare New List(named "Fruits")
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
 
+print("Our fruit selection:") # Print Last Message
+for fruit in fruits:          # For Loop To Check List Items
+    print(fruit)              # Print Fruit(item) Name
