e9401c05bc65bcc531aff066c60c11cd57156674
Yasin samy <78241714+y3sin@users.noreply.github.com>
2021-10-21 09:40:09 -0700

    Update Guessing-Game-Challenge.py

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
@@ -1,1 +1,8 @@
+food = "koshary"                          # Declaring My Favorite Food
+guess = input("What's my favorite food?") # Ask The User
+if guess == food:                         # Check If Guessing True
+    print("Yep! So amazing!")             # Print Sucess Message
+else:                                             # Or #
+    print("Yuck! That's not it!")         # Print Field Message
 
+print("Thanks for playing!")              # Say Thanks To User   
