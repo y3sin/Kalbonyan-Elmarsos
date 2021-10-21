17d9210df5f2f2b7cb21f781b6a7687f9df5e575
Yasin samy <78241714+y3sin@users.noreply.github.com>
2021-10-21 09:55:25 -0700

    Update Working-with-collections-challenge.py

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
@@ -1,1 +1,33 @@
+# Nearest stars to Earth
+# star1 = 'Sol'
+# star2 = 'Alpha Centauri'
+# star3 = 'Barnard'
+# star4 = 'Wolf 359'
+stars = [       # Replace Variables' Value In A List
+    'Sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359',
+]
+print(stars[3]) # Print Fourth Nearest Star(at index [3])
+#>>> Wolf 359
 
+# Highest peak on each tectonic plate
+# African = 'Kilimanjaro'
+# Antarctic = 'Vinson'
+# Australian = 'Puncak Jaya'
+# Eurasian = 'Everest'
+# North_American = 'Denali'
+# Pacific = 'Mauna Kea'
+# South_American = 'Aconcagua'
+peaks ={                # Replace Variables' Value In A Dictionary
+    'African'       : 'Kilimanjaro',
+    'Antarctic'     : 'Vinson',
+    'Australian'    : 'Puncak Jaya',
+    'Eurasian'      : 'Everest',
+    'North_American': 'Denali',   
+    'Pacific'       : 'Mauna Kea',
+    'South_American': 'Aconcagua',
+}
+print(peaks['Pacific']) #Print Name Of Highest Peak On Pacific Plate(named 'pacific')
+#>>> Mauna Kea
