41d6895bda2728f1ab4b6b95868d1e50c76dbaf1
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-07 10:17:37 -0800

    Update if.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/if.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/if.js
@@ -1,1 +1,10 @@
-The Modern JavaScript Bootcamp
+//Setting The Age
+let age = 5;
+//Printing The Messages
+if(age <= 7) {
+  console.log(`You will get a child pricing.`);
+} else if(age >=65) {
+  console.log(`You will get a senior discount.`);
+} else {
+  console.log(`You are not eligible for a discount.`);
+}
