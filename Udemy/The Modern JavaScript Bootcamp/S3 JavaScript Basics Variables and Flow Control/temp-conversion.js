c49463182623b7b249adbd95be75aa3c33c2a193
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-07 12:15:10 -0800

    Update temp-conversion.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
@@ -1,1 +1,7 @@
-The Modern JavaScript Bootcamp
+let fahrenhait = 32 // [ 0c ] - [ 273.15k ]
+let calsius = (fahrenhait - 32) * 5 / 9
+let kelvin = (fahrenhait + 459.67) * 5 / 9
+
+console.log("fahrenhait: "+ fahrenhait + "\n\n");
+console.log("calsius:    "+ calsius + "\n\n");
+console.log("kelvin:     "+ kelvin);
