97d09efdd02325f1cd67b77918325bac02123d6a
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-07 11:45:48 -0800

    Update scope-part2.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/scope-part2.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/scope-part2.js
@@ -1,1 +1,17 @@
-The Modern JavaScript Bootcamp
+let name = "Yasin";
+console.log("1) " + name);//        ==> "Yasin"    (global)
+
+if (true) {
+  let name = "Samy";
+  console.log("2) " + name);//      ==> "Samy"     (local)
+
+  if (true) {
+    let name = "Mohammed";
+    console.log("3) " + name);//    ==> "Mohammed" (local)
+  }
+}
+console.log("4) " + name);//        ==> "Yasin"    (global)
+
+if (true) {
+  console.log("5) " + name);//      ==> "Yasin" (global)
+}
