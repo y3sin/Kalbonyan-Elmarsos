dc0329187546f7cdc01a5a938aeb1d0a2afb770f
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-07 10:30:15 -0800

    Update if-advanced.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/if-advanced.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/if-advanced.js
@@ -1,1 +1,10 @@
-The Modern JavaScript Bootcamp
+//Setting The Temperature
+let temp = 45;
+//Printing The Messages
+if(temp <= 31) {
+  console.log(`It is hot outside!`);
+} else if(temp >= 120) {
+  console.log(`It is freezing outside!`);
+} else {
+  console.log(`Go for it, It is pretty nice!`);
+}
