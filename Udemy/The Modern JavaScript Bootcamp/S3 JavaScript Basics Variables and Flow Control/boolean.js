b8930ec299856ddd62485af8e16e150f904371ec
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-07 09:47:17 -0800

    Update boolean.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/boolean.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/boolean.js
@@ -1,1 +1,17 @@
-The Modern JavaScript Bootcamp
+//Setting The Age
+let age = 5;
+//Declare The Case Of Sale Variable
+let saleCase;
+//Calculating The Case Of Sale
+if(age <= 7 || age >= 65){
+  saleCase = true;
+} else {
+  saleCase = false;
+}
+//Printing The Message
+console.log(`Sale: ${saleCase}`);
+if(age <= 7) {
+  console.log(`You Are Child`);
+} else if(age >=65) {
+  console.log(`You Are Senior`);
+}
