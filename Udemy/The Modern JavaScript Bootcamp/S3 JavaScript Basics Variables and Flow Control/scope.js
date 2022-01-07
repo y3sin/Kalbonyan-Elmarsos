96ddb056bc8df87707510cfac8464a0aba3d8ef2
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-07 11:29:56 -0800

    Update scope.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/scope.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/scope.js
@@ -1,1 +1,20 @@
-The Modern JavaScript Bootcamp
+let varOne = "varOne";
+
+if (true) {
+  console.log(varOne);
+  let varTwo = "varTwo";
+  console.log(varTwo);
+
+  if (true) {
+    let varFour = "varFour";
+  }
+}
+
+if (true) {
+  let varThree = "varThree";
+}
+
+console.log(varOne);
+console.log(varTwo);
+console.log(varThree);
+console.log(varFour);//       ==> Not Apper (out scope)
