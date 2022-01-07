2ef57a5a63b70acea1fa1ca4ee95942076139113
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-07 09:21:06 -0800

    Update Numbers.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/Numbers.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/Numbers.js
@@ -1,1 +1,10 @@
-The Modern JavaScript Bootcamp
+//Student Score  ==> 18 (in this case)
+let studentScore = 18;
+//High Score     ==> 20 (in this case)
+let HighScore = 20;
+//counting The Percent    ==> 90% (in this case)
+  //Declare A Variable To Store The Percent
+  let Percent;
+Percent = (18/20) * 100
+//Printing The Result
+console.log(`Result Is ${Percent}%`)
