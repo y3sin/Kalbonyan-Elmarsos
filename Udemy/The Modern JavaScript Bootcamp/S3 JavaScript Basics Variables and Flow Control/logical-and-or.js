63047843140beea6cc90d82a0cdfdd7099dc39f2
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-07 10:53:43 -0800

    Update logical-and-or.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/logical-and-or.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/logical-and-or.js
@@ -1,1 +1,11 @@
-The Modern JavaScript Bootcamp
+//Print If Visitors Are Vegans
+let isGuestOneVegan = true;
+let isGuestTwoVegan = false;
+//Printing The Messages
+if(isGuestOneVegan && isGuestTwoVegan) {
+  console.log(`Only offer up vegan dishes.`);
+} else if(!isGuestOneVegan && !isGuestTwoVegan) {
+  console.log(`Offer up any thing on the menue.`);
+} else {
+  console.log(`Make sure to offer up some vegan options.`);
+}
