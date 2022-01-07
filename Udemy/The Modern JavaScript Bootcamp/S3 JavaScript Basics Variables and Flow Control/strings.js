ceaa643f88a448c69ae511ae9f3365657e914112
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-07 11:08:07 -0800

    Update strings.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/strings.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/strings.js
@@ -1,1 +1,8 @@
-The Modern JavaScript Bootcamp
+//Declare The City
+let city = "Cairo";
+//Declare The Country
+let country = "Egypt";
+//Merging The City And The Country
+let location = city + ", " + country;
+//Output The Location
+console.log("Location: " + location)
