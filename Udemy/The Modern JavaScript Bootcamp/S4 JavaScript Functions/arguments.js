6e71d83b1ba022d362eaae4d0a8b16da7dcd2d7c
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-08 14:28:10 -0800

    Update arguments.js

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/arguments.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/arguments.js
@@ -1,1 +1,17 @@
-The Modern JavaScript Bootcamp
+// Declare The Default Value Of Total
+let tipTotal = 100;
+// Declare The Percent Variable
+let tipPercent;
+// Declare The Counting Function
+let tipCount = function (total, percent = 0.2) { return percent * total };
+
+// Print The Function With The Default Value For Tip Percent [20%]
+console.log("The needed tip is: " +       tipCount(tipTotal)       + "$");   // 20% * 100$ ==> [ 20$ ]
+
+// Setting TipPercent Value To [ 15% ] And Print The Function
+tipPercent = 0.15;
+console.log("The needed tip is: " + tipCount(tipTotal, tipPercent) + "$");   // 15% * 100$ ==> [ 15$ ]
+
+// Setting TipTotal Value To [ 200$ ] And Print The Function
+tipTotal = 200;
+console.log("The needed tip is: " + tipCount(tipTotal, tipPercent) + "$");   // 15% * 200$ ==> [ 30$ ]
