b8cb6598b3adff9918d74bf2dce60d7dd3186703
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-08 13:49:35 -0800

    Update undefined-null.js

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/undefined-null.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/undefined-null.js
@@ -1,1 +1,24 @@
-The Modern JavaScript Bootcamp
+// Undefined For Variable
+let name = "Yasin"
+
+if (name === undefined) {
+  console.log("Please provide a name");
+} else {
+  console.log(name);
+}
+
+// Undefined For Function Arguments
+// Undefined As Function Return Default Value
+let square = function (num) {
+  console.log(num);
+}
+
+let result = square();
+console.log(result);
+
+// Null As Assined Value
+let age = 27;
+
+age = null;
+
+console.log(age);
