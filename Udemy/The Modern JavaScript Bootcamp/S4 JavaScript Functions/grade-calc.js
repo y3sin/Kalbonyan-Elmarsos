2f1d7a7dcc7189fc0083cf795bbe5001b6cf3e53
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-08 15:03:02 -0800

    Update grade-calc.js

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/grade-calc.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/grade-calc.js
@@ -1,1 +1,23 @@
-The Modern JavaScript Bootcamp
+// Students Score, Total Possible Score
+// 15/20 ==> You got a C (75%)
+// A 90-100, B 80-89, C 70-79, D 60-69, F 0-59
+
+let gradeCalc = function (score, totalScore) {
+  let percent = (score / totalScore) * 100;
+  let letterGrade = '';
+
+  if (percent >= 90) {
+    letterGrade = 'A';
+  } else if (percent >= 80) {
+    letterGrade = 'B';
+  } else if (percent >= 70) {
+    letterGrade = 'C';
+  } else if (percent >= 60) {
+    letterGrade = 'D';
+  } else {
+    letterGrade = 'F';
+  }
+}
+
+let result = letterGrade(9, 20);
+console.log("Your result is: " + result);
