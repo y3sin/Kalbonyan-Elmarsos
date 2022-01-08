ce430ed2b22f9aa53ab637fa1573c74246d81e25
Yasin samy <78241714+y3sin@users.noreply.github.com>
2022-01-08 14:47:07 -0800

    Update function-scope.js

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/function-scope.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/function-scope.js
@@ -1,1 +1,18 @@
-The Modern JavaScript Bootcamp
+// Global Scope (convertFahrenheitToCelsius, tempOne, tempTwo)
+  // Local Scope (fahrenheit, celsius)
+    // Local Scope (isFreezing)
+let convertFahrenheitToCelsius = function (fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9;
+
+  if (celsius <= 0) {
+    let isFreezing = true;
+  }
+
+  return celsius;
+}
+
+let tempOne = convertFahrenheitToCelsius(32);
+let tempTwo = convertFahrenheitToCelsius(68);
+
+console.log("Temp one is: " + tempOne + "  celsius");
+console.log("Temp two is: " + tempTwo + " celsius");
